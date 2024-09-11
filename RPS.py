19798302779244857510bf251ad2563c8df677e6
Masoud Abrishami <masoud.abrishami@gmail.com>
2024-09-11 06:58:23 +0000

    new change

--- RPS.py
+++ RPS.py
@@ -1,10 +1,23 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+import random
 
 def player(prev_play, opponent_history=[]):
+    # Keep track of the opponent's history
     opponent_history.append(prev_play)
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    if len(opponent_history) == 0 or prev_play == '':
+        # First move, choose randomly
+        return random.choice(['R', 'P', 'S'])
 
-    return guess
+    # Analyze the opponent's last moves
+    last_play = opponent_history[-1]
+    
+    # Simple counter-strategy: beat the opponent's last move
+    if last_play == 'R':
+        return 'P'  # Paper beats Rock
+    elif last_play == 'P':
+        return 'S'  # Scissors beat Paper
+    elif last_play == 'S':
+        return 'R'  # Rock beats Scissors
+
+    # Default to random choice if something goes wrong
+    return random.choice(['R', 'P', 'S'])
